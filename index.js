3cc0d7f457a90ae3f6ab87864cb1f2817f697a00
Chris <orthagonal1@gmail.com>
2016-05-18 14:40:52 -0500

    refactor auto statement, added dependency ordering, tests for vision load, updated eslint packages to latest versions

--- index.js
+++ index.js
@@ -19,174 +19,160 @@ module.exports = (Hapi, options, allDone) => {
     options = {};
   }
   options.configPath = options.configPath || `${cwd}/conf`;
-  // load chain:
-  const loadConfig = (done) => {
-    const confiOptions = {
-      path: options.configPath
-    };
-    if (options.env) {
-      confiOptions.env = options.env;
-    }
-    try {
-      const config = confi(confiOptions);
-      return done(null, config);
-    } catch (exc) {
-      return done(exc);
-    }
-  };
-
-  const loadServer = (done, result) => {
-    const config = result.config;
-    const serverConfig = _.cloneDeep(config.server || {});
-    const connection = config.connection || {};
-    if (serverConfig.cache) {
-      serverConfig.cache.engine = requireCwd(serverConfig.cache.engine);
-    }
-    if (process.env.PORT) {
-      connection.port = process.env.PORT;
-    }
-    const server = new Hapi.Server(serverConfig);
-    server.connection(connection);
-    server.settings.app = config;
-    done(null, server);
-  };
-
-  const loadLogging = (done, result) => {
-    const server = result.server;
-    const config = result.config;
-    if (!config.logging) {
-      return done(null, server, config);
-    }
-    const reporters = [];
-    const keys = [];
-    _.forIn(config.logging.reporters, (value, key) => {
-      if (value === false || value._enabled === false) {
-        return;
-      }
-      delete value._enabled;
-      keys.push(key);
-      value.reporter = requireCwd(`good-${key}`);
-      reporters.push(value);
-    });
-    if (reporters.length !== 0) {
-      config.logging.reporters = reporters;
-      server.register({
-        register: requireCwd('good'),
-        options: config.logging
-      }, (err) => {
-        server.log(['hapi-confi'], { message: 'good reporters loaded', reporters: keys });
-        done(err, server, config);
-      });
-    } else {
-      return done(null, server, config);
-    }
-  };
-
-  const loadAuth = (done, result) => {
-    const server = result.server;
-    const config = result.config;
-    if (!config.authPlugins) {
-      return done(null, server, config);
-    }
-    async.forEachOfSeries(config.authPlugins, (value, key, eachDone) => {
-      if (typeof value === 'undefined' || value === null) {
-        value = {};
-      }
-      if (value === false || value._enabled === false) {
-        return eachDone();
-      }
-      delete value._enabled;
-      server.log(['hapi-confi'], { message: 'auth plugin loaded', plugin: key, options: value });
-      server.register({
-        register: requireCwd(key),
-        options: value
-      }, eachDone);
-    }, (err) => {
-      done(err, server, config);
-    });
-  };
-
-  const loadStrategery = (done, result) => {
-    const server = result.server;
-    const config = result.config;
-    _.forIn(config.strategies, (value, name) => {
-      server.log(['hapi-confi'], { message: 'strategy loaded', strategy: name, options: value });
-      const profileFn = _.get(value, 'options.provider.profile');
-      if (typeof profileFn === 'string') {
-        value.options.provider.profile = (credentials, params, get, callback) => {
-          server.methods[profileFn](credentials, params, get, callback);
-        };
-      }
-      server.auth.strategy(name, value.scheme, value.mode, value.options);
-    });
-    done(null, server, config);
-  };
-
-  const loadPlugins = (done, result) => {
-    const server = result.server;
-    const config = result.config;
-    if (!config.plugins) {
-      return done(null, server, config);
-    }
-    let pluginArr = [];
-    _.forIn(config.plugins, (value, key) => {
-      if (value === null) {
-        value = {};
-      }
-      if (value === false) {
-        return;
-      }
-      if (value._enabled === false) {
-        return;
-      }
-      value._name = key;
-      pluginArr.push(value);
-    });
-    pluginArr = _.sortBy(pluginArr, '_priority');
-    async.eachSeries(pluginArr, (plugin, eachDone) => {
-      const name = plugin._name;
-      delete plugin._name;
-      delete plugin._enabled;
-      delete plugin._priority;
-      server.log(['hapi-confi'], { message: 'plugin loaded', plugin: name, options: plugin });
-      server.register({
-        register: requireCwd(name),
-        options: plugin
-      }, eachDone);
-    }, (err) => {
-      done(err, server, config);
-    });
-  };
-
-  const loadViews = (done, result) => {
-    const server = result.server;
-    const config = result.config;
-    if (config.views) {
-      _.forIn(config.views.engines, (engine, ext) => {
-        if (typeof engine === 'string') {
-          config.views.engines[ext] = requireCwd(engine);
-        }
-      });
-      server.views(config.views);
-      server.log(['hapi-confi'], { message: 'views configured' });
-    }
-    done(null, server, config);
-  };
 
   async.auto({
-    config: loadConfig,
-    server: ['config', loadServer],
+    config: (done) => {
+      const confiOptions = {
+        path: options.configPath
+      };
+      if (options.env) {
+        confiOptions.env = options.env;
+      }
+      try {
+        const config = confi(confiOptions);
+        return done(null, config);
+      } catch (exc) {
+        return done(exc);
+      }
+    },
+    server: ['config', (done, result) => {
+      const config = result.config;
+      const serverConfig = _.cloneDeep(config.server || {});
+      const connection = config.connection || {};
+      if (serverConfig.cache) {
+        serverConfig.cache.engine = requireCwd(serverConfig.cache.engine);
+      }
+      if (process.env.PORT) {
+        connection.port = process.env.PORT;
+      }
+      const server = new Hapi.Server(serverConfig);
+      server.connection(connection);
+      server.settings.app = config;
+      done(null, server);
+    }],
     beforeHook: ['server', (done, result) => {
       if (typeof options.before !== 'function') {
         return done();
       }
       options.before(result.server, result.config, done);
     }],
-    log: ['server', loadLogging],
-    auth: ['server', loadAuth],
-    strategies: ['server', loadStrategery],
-    plugins: ['server', loadPlugins],
-    views: ['server', loadViews]
+    log: ['beforeHook', (done, result) => {
+      const server = result.server;
+      const config = result.config;
+      if (!config.logging) {
+        return done(null, server, config);
+      }
+      const reporters = [];
+      const keys = [];
+      _.forIn(config.logging.reporters, (value, key) => {
+        if (value === false || value._enabled === false) {
+          return;
+        }
+        delete value._enabled;
+        keys.push(key);
+        value.reporter = requireCwd(`good-${key}`);
+        reporters.push(value);
+      });
+      if (reporters.length !== 0) {
+        config.logging.reporters = reporters;
+        server.register({
+          register: requireCwd('good'),
+          options: config.logging
+        }, (err) => {
+          server.log(['hapi-confi'], { message: 'good reporters loaded', reporters: keys });
+          done(err, server, config);
+        });
+      } else {
+        return done(null, server, config);
+      }
+    }],
+    auth: ['log', (done, result) => {
+      const server = result.server;
+      const config = result.config;
+      if (!config.authPlugins) {
+        return done(null, server, config);
+      }
+      async.forEachOfSeries(config.authPlugins, (value, key, eachDone) => {
+        if (typeof value === 'undefined' || value === null) {
+          value = {};
+        }
+        if (value === false || value._enabled === false) {
+          return eachDone();
+        }
+        delete value._enabled;
+        server.log(['hapi-confi'], { message: 'auth plugin loaded', plugin: key, options: value });
+        server.register({
+          register: requireCwd(key),
+          options: value
+        }, eachDone);
+      }, (err) => {
+        done(err, server, config);
+      });
+    }],
+    strategies: ['auth', (done, result) => {
+      const server = result.server;
+      const config = result.config;
+      _.forIn(config.strategies, (value, name) => {
+        server.log(['hapi-confi'], { message: 'strategy loaded', strategy: name, options: value });
+        const profileFn = _.get(value, 'options.provider.profile');
+        if (typeof profileFn === 'string') {
+          value.options.provider.profile = (credentials, params, get, callback) => {
+            server.methods[profileFn](credentials, params, get, callback);
+          };
+        }
+        server.auth.strategy(name, value.scheme, value.mode, value.options);
+      });
+      done(null, server, config);
+    }],
+    plugins: ['strategies', (done, result) => {
+      const server = result.server;
+      const config = result.config;
+      if (!config.plugins) {
+        return done(null, server, config);
+      }
+      let pluginArr = [];
+      _.forIn(config.plugins, (value, key) => {
+        if (value === null) {
+          value = {};
+        }
+        if (value === false) {
+          return;
+        }
+        if (value._enabled === false) {
+          return;
+        }
+        value._name = key;
+        pluginArr.push(value);
+      });
+      pluginArr = _.sortBy(pluginArr, '_priority');
+      async.eachSeries(pluginArr, (plugin, eachDone) => {
+        const name = plugin._name;
+        delete plugin._name;
+        delete plugin._enabled;
+        delete plugin._priority;
+        server.log(['hapi-confi'], { message: 'plugin loaded', plugin: name, options: plugin });
+        server.register({
+          register: requireCwd(name),
+          options: plugin
+        }, eachDone);
+      }, (err) => {
+        done(err, server, config);
+      });
+    }],
+    views: ['plugins', 'strategies', 'beforeHook', (done, result) => {
+      const server = result.server;
+      const config = result.config;
+      if (config.views) {
+        _.forIn(config.views.engines, (engine, ext) => {
+          if (typeof engine === 'string') {
+            config.views.engines[ext] = requireCwd(engine);
+          }
+        });
+        server.views(config.views);
+        server.log(['hapi-confi'], { message: 'views configured' });
+      }
+      done(null, server, config);
+    }]
   }, (autoErr, result) => {
     if (autoErr) {
       return allDone(autoErr);
--- test/server-test.js
+++ test/server-test.js
@@ -60,6 +60,26 @@ lab.test('views are configured ', (done) => {
     done();
   });
 });
+
+lab.test('test server can load vision view engine ', (done) => {
+  hapiconfi(Hapi, { configPath: `${__dirname}/conf3` }, (err, server) => {
+    code.expect(err).to.equal(null);
+    let success = false;
+    try {
+      server.views({
+        engines: {
+          html: 'handlebars'
+        }
+      });
+    } catch (exc) {
+      success = true;
+    }
+    code.expect(success).to.equal(true);
+    code.expect(server.registrations.vision).to.not.equal(undefined);
+    done();
+  });
+});
+
 lab.test('strategies are configured ', (done) => {
   hapiconfi(Hapi, { configPath: `${__dirname}/conf` }, (err, server) => {
     let success = false;
