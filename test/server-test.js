d52180a256f11b5ca4f329c6175e622d52c1c180
orthagonal <orthagonal1@gmail.com>
2017-12-31 21:31:18 -0600

    updated

--- test/server-test.js
+++ test/server-test.js
@@ -3,7 +3,7 @@ const hapiconfi = require('../');
 const Hapi = require('hapi');
 const code = require('code');
 const lab = exports.lab = require('lab').script();
-
+/*
 lab.test('test server is initialized ', async () => {
   const { server, config } = await hapiconfi(Hapi, { configPath: `${__dirname}/conf` });
   await server.start();
@@ -43,4 +43,20 @@ lab.test('test server can load vision view engine ', async() => {
   }
   code.expect(success).to.equal(true);
   code.expect(server.registrations.vision).to.not.equal(undefined);
+});
+
+lab.test('loads plugins in order of dependencies ', async() => {
+  try {
+    const { server, config } = await hapiconfi(Hapi, { env: 'broke', configPath: `${__dirname}/conf_plugins` });
+    // the 'broke' config has the wrong dependency order and will crash:
+  } catch (err) {
+    console.log('---------------------------')
+    console.log(err)
+    code.expect(err).to.not.equal(null);
+  }
+});
+*/
+lab.test('logs to console if it sees the deprecated "_priority" field ', async() => {
+  const { server, config } = await hapiconfi(Hapi, { env: 'report', configPath: `${__dirname}/conf_plugins` });
+  done();
 });
